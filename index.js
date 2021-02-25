8ede6ecaefa6d450a3682f0014b462f01b2666b7
Juarez Goulart <goulartgarcia12@gmail.com>
2021-02-25 19:59:38 -0300

    touch commit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ lightMode.addEventListener('click', () => {
     document.documentElement.classList.toggle('dark-mode')
 })
 
+lightMode.addEventListener('touchstart', () => {
+    document.documentElement.classList.toggle('dark-mode')
+})
 /*lightMode.addEventListener('click', () => {
     document.getElementById('img').src = "./assets/darkness-dark.png";
 })*/
