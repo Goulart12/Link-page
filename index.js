08d2e65a34b8132593ef3e607dc703a3ddd83e29
Juarez Goulart <goulartgarcia12@gmail.com>
2021-03-05 17:02:19 -0300

    tentativa 3

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const lightMode = document.querySelector('#light-mode');
 lightMode.addEventListener('click', () => {
     document.documentElement.classList.toggle('dark-mode')
 })
+
+const mobileBtn = document.querySelector('#mobileButton');
+
+mobileBtn.addEventListener('touchstart', () => {
+    document.documentElement.classList.toggle('dark-mode')
+})
+
     
 /*lightMode.addEventListener('click', () => {
     document.getElementById('img').src = "./assets/darkness-dark.png";
 })*/
 
-document.addEventListener("touchstart", alterarMobile)
-
-
-function alterarMobile() {
-    document.documentElement.classList.toggle('dark-mode')
-}
 
 function alterarImagem() {
     document.getElementById('img').src = "./assets/darkness-dark.png";
