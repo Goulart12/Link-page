4e06695bc8189144acfa833f8851fd313268aaac
Juarez Goulart <goulartgarcia12@gmail.com>
2021-03-05 16:48:38 -0300

    tent

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const lightMode = document.querySelector('#light-mode');
 lightMode.addEventListener('click', () => {
     document.documentElement.classList.toggle('dark-mode')
 })
-
-/*lightMode.addEventListener('touchstart', alterarMobile())
-
-/*function alterarMobile () {
-    document.documentElement.classList.toggle('dark-mode')
-}
     
 /*lightMode.addEventListener('click', () => {
     document.getElementById('img').src = "./assets/darkness-dark.png";
 })*/
 
-document.addEventListener("touchstart", alterarImagem)
+document.addEventListener("touchstart", alterarMobile)
+document.addEventListener("touchmove", alterarMobile)
+
+function alterarMobile() {
+    document.documentElement.classList.toggle('dark-mode')
+}
 
 function alterarImagem() {
     document.getElementById('img').src = "./assets/darkness-dark.png";
